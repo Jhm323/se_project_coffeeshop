231437b4a71484f6e80b3ec64bd0fc7e666af7c3
holden <james.holden.moore@gmail.com>
2026-03-17 14:17:46 -0400

    refactor server.js to use ES modules

--- server/server.js
+++ server/server.js
@@ -6,36 +6,25 @@ import reservationsRouter from "./routes/reservations.js";
 import menuRouter from "./routes/menu.js";
 import contactRouter from "./routes/contact.js";
 
-require("dotenv").config();
-
-const express = require("express");
-const cors = require("cors");
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/reservations", reservationsRouter);
 app.use("/menu", menuRouter);
 app.use("/contact", contactRouter);
 
-// 404
 app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
 });
 
-// Error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Something went wrong!" });
 });
 
-// Start server
-const PORT = process.env.PORT || 5001;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
