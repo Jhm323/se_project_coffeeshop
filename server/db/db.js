79c1474d3eb56631eb97d6e9b704f83fdb68b36c
holden <james.holden.moore@gmail.com>
2026-03-17 14:33:14 -0400

    update export const pool in db.db.js

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-const pool = new Pool({
+export const pool = new Pool({
   user: process.env.DB_USER || "postgres",
   host: process.env.DB_HOST || "localhost",
   database: process.env.DB_NAME || "coffee_shop",
