e338be78a79f8fc2e9d4c6abf1feb8c3d983c908
holden <james.holden.moore@gmail.com>
2026-03-18 14:14:11 -0400

    update db.js to connect with render

--- server/db/db.js
+++ server/db/db.js
@@ -4,14 +4,21 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-export const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "coffee_shop",
-  password: process.env.DB_PASS || "", // blank if no password
-  port: process.env.DB_PORT || 5432,
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    })
+  : new Pool({
+      user: process.env.DB_USER || "postgres",
+      host: process.env.DB_HOST || "localhost",
+      database: process.env.DB_NAME || "coffee_shop",
+      password: process.env.DB_PASS || "",
+      port: process.env.DB_PORT || 5432,
+    });
 
 pool.on("connect", () => {
   console.log("Connected to Postgres!");
 });
+
+export { pool };
